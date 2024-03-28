24ff159ff1396279e078318477cef473b69f9326
Sergio Henrique <114114763+Sergin03@users.noreply.github.com>
2024-03-27 21:11:11 -0300

    Update trabalho_individual.py

--- trabalho_individual.py
+++ trabalho_individual.py
@@ -1,34 +1,51 @@
-
+# Lista para armazenar os registros dos candidatos
 registros_candidatos = []
 
-# Loop para cadastrar os candidatos
-while True:
-    candidato = input("Digite o nome do candidato (ou digite 'sair' para encerrar): ")
-    resultadoString = 'ex_tx_px_sx'
-    if candidato.lower() == 'sair':
-        break
+print('\n')
+print('Escolha uma opção abaixo: ')
+print('\n')
+opcao = input(' 1 - Cadastrar Candidato \n 2 - Exibir Nota \n 3 - Sair\n')
+print('\n\n')
 
-    # Validar e capturar valores para entrevista, teste teórico, teste prático, soft skills e média
-    while True:
+while opcao == "1" or opcao == "2":
+    if opcao == "1":
+
         try:
-            entrevista = int(input("Digite o valor da entrevista (apenas números inteiros): "))
-            teste_teorico = int(input("Digite o valor do teste teorico (apenas números inteiros): "))
-            teste_pratico = int(input("Digite o valor do teste pratico (apenas números inteiros): "))
-            soft_skills = int(input("Digite o valor do soft_skills do candidato (apenas números inteiros): "))
-            break
+            entrevista = input("Digite a nota da entrevista (apenas números): ")
+            teste_teorico = input("Digite a nota do teste teorico (apenas números): ")
+            teste_pratico = input("Digite a nota do teste pratico (apenas números): ")
+            soft_skills = input("Digite a nota do soft_skills do candidato (apenas números): ")
+
         except ValueError:
             print("Por favor, digite apenas números inteiros.")
 
-    # Adicionar o registro à lista de registros de candidatos
-    registro = f"Candidato: {candidato}, Entrevista: {entrevista}, Teste Teórico: {teste_teorico}, Teste Prático: {teste_pratico}, Soft Skills: {soft_skills} "
-    registros_candidatos.append(registro)
-    print (f"e{entrevista}_t{teste_teorico}_p{teste_pratico}_s{soft_skills}")
+        registro = {
+            'Entrevista': entrevista,
+            'Teste Teórico': teste_teorico,
+            'Teste Prático': teste_pratico,
+            'Soft Skills': soft_skills,
+        }
+        registros_candidatos.append(registro)
+        print(f"e{entrevista}_t{teste_teorico}_p{teste_pratico}_s{soft_skills}")
 
-    parar = input('Deseja adicionar outro candidato? (s/n): ')
-    if parar.lower() == 'n':
-        break
+        opcao = input(' 1 - Cadastrar Candidato \n 2 - Exibir Nota \n 3 - Sair\n')
+    elif opcao =='2':
 
-# Exibir todos os registros de candidatos
-print("\nRegistros de candidatos:")
-for registro in registros_candidatos:
-    print(registro)
+        nota_entrevista = int(input("Digite a nota mínima na entrevista: "))
+        nota_teste_teorico = int(input("Digite a nota mínima no teste teórico: "))
+        nota_teste_pratico = int(input("Digite a nota mínima no teste prático: "))
+        nota_soft_skills = int(input("Digite a nota mínima em soft skills: "))
+        
+        for registro in registros_candidatos:
+            nota_entrevista_candidato = int(registro['Entrevista'])
+            nota_teste_teorico_candidato = int(registro['Teste Teórico'])
+            nota_teste_pratico_candidato = int(registro['Teste Prático'])
+            nota_soft_skills_candidato = int(registro['Soft Skills'])
+            4
+            if (nota_entrevista_candidato >= nota_entrevista and
+                nota_teste_teorico_candidato >= nota_teste_teorico and
+                nota_teste_pratico_candidato >= nota_teste_pratico and
+                nota_soft_skills_candidato >= nota_soft_skills):
+                
+                print(f"e{nota_entrevista_candidato}_t{nota_teste_teorico_candidato}_p{nota_teste_pratico_candidato}_s{nota_soft_skills_candidato}")
+        opcao = input(' 1 - Cadastrar Candidato \n 2 - Exibir Nota \n 3 - Sair\n')
